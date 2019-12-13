101ea1a041405fd6b82c4f14e225f012dfedb140
tbp <tianbeiping_cd@keruyun.com>
2019-12-13 19:55:32 +0800

    备注，请求

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ spring boot 2  默认使用 logback
 >
 > mybatisGenerate.gradle 是一种备用方案  --->  generate/generatorConfig.xml
 >
+
+
+```text
+@Bean
+public MapperScannerConfigurer mapperScannerConfigurer(){
+    MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+    mapperScannerConfigurer.setBasePackage("com.xxxx.dao");
+    Properties propertiesMapper = new Properties();
+    //通用mapper位置，不要和其他mapper、dao放在同一个目录
+    propertiesMapper.setProperty("mappers", "com.xxxx.Mapper");
+    propertiesMapper.setProperty("notEmpty", "false");
+    //主键UUID回写方法执行顺序,默认AFTER,可选值为(BEFORE|AFTER)
+    propertiesMapper.setProperty("ORDER","BEFORE");
+    mapperScannerConfigurer.setProperties(propertiesMapper);
+return mapperScannerConfigurer;
+
+```
--- src/main/java/com/tbp/spring/boot/mybatis/template/SpringBootMybatisTemplateApplication.java
+++ src/main/java/com/tbp/spring/boot/mybatis/template/SpringBootMybatisTemplateApplication.java
@@ -1,5 +1,6 @@
 package com.tbp.spring.boot.mybatis.template;
 
+import com.tbp.spring.boot.mybatis.template.mapper.BaseMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -8,7 +9,10 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @EnableTransactionManagement
 //@EnableJpaRepositories
-//@MapperScan("com.tbp.spring.boot.mybatis.template.mapper")
+@MapperScan(
+        basePackages = "com.tbp.spring.boot.mybatis.template.mapper",
+        markerInterface = BaseMapper.class
+)
 public class SpringBootMybatisTemplateApplication {
 
     public static void main(String[] args) {
